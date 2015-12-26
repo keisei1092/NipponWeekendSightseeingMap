2228030090ef974fdd50f6b9ea44f3b86743e90a
keisei_1092 <keisei_1092@yahoo.co.jp>
2015-12-27 03:59:04 +0900

    WIP: メインクラス作成

---
+++ src/jp/ac/dendai/im/nipponweekendsightseeingmap/Main.java
@@ -0,0 +1,13 @@
+package jp.ac.dendai.im.nipponweekendsightseeingmap;
+
+/**
+ * Created by keisei on 12/22/15.
+ */
+public class Main {
+    public static void main(String[] args) {
+        Weather w = new Weather();
+        boolean isSunny[] = w.checkWeekend();
+
+        // TODO: 晴れの地点をリストで持って、pubDateの昇順でソートして表示
+    }
+}
